d4d88c74e5488b87dad90382bfb506771bcef2c8
Dennis Huang <dennishuang@oursong.com>
2020-10-16 15:52:10 +0800

    adjust time auto

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo cp adjust.time.auto.plist /Library/LaunchDaemons/
---
+++ stop.sh
@@ -0,0 +1,2 @@
+sudo launchctl stop adjust.time.auto.plist
+sudo launchctl unload /Library/LaunchDaemons/adjust.time.auto.plist
